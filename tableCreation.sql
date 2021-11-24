50236d99b36a6cf29d25f6bcc862b2cd8dcef0eb
gabe <gabevenberg@gmail.com>
2021-11-24 11:11:45 -0600

    fixed some formatting, added starting rows.
    added starting rows to users and settings tables.

--- tableCreation.sql
+++ tableCreation.sql
@@ -2,37 +2,42 @@
 
 
 create table if not exists items(
-	itemID serial,
-	purchaseLink varchar(512),
-	itemDesc varchar(256),
+	itemID serial primary key,
+	purchaseLink text,
+	itemDesc text,
 	priority smallint default 10,
 	thumbnail varchar(1024),
 	QTYDesired smallint default 1,
-	constraint items_pkey primary key (itemID)
 );
 
 create table if not exists users(
-	userID serial,
-	PWhash varchar(128) not null,
-	email varchar(256) not null,
+	userID serial primary key,
+	PWhash text not null,
+	username varchar(256) not null unique,
 	privLevel smallint default 0,
-	constraint users_pkey primary key (userID)
 );
 
 create table if not exists purchase(
-	purchaseID serial,
+	purchaseID serial primary key,
 	QTYpurchased smallint defualt 1,
 	datePurchased timestamp,
 	userID integer,
 	itemID integer not null,
-	constraint purchase_pkey primary key (purchaseID),
 	constraint userid_record foreign key (userID) references users (userID)
 	constraint userid_record foreign key (itemID) references items (itemID)
 );
 
 --for assignment reqs, might move this into a config file later
 create table if not exists setting(
-	field varchar(64),
-	value varchar(256),
-	constraint setting_pkey primary key (purchaseID)
+	field varchar(64) primary key,
+	value text,
+);
+
+insert into setting(field, value) values
+	('usersCanSeeAlreadyPurchased', 'true'),
+	('theme', 'dark');
+
+create extention pgcrypto;
+insert into users(PWhash, username, privLevel) values (
+	crypt('badPasword', gen_salt('bf')),
 );
