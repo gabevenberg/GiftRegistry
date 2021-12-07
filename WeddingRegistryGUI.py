bccc5cac84bf9ae817f467723cab568a03a4ad84
Mindarius <67028450+Mindarius@users.noreply.github.com>
2021-12-06 19:47:39 -0600

    login page updates
    tkinter import now aliased to TK so I don't need to type as much
    
    Added window title (GiftRegistry) and initial size (647x400)
    
    Added attempt_login method which uses authenticate user to interact with the UI rather than just being 'unimplemented'. Displays error message if incorrect login, otherwise sends to main page
    
    initial page switched to login rather than main
    
    TODO: logging in opens a new window, it should either close the previous window, or instead change the window to the new one. Keeping the login window open looks dumb.

--- WeddingRegistryGUI.py
+++ WeddingRegistryGUI.py
@@ -1,35 +1,47 @@
-import tkinter
+import tkinter as TK
 
 display_page_query="select * from items where QTYDesired>0;"
 def send_login_page():
-    top=tkinter.Tk()
-    button=tkinter.Button(top,text="login",command=lambda:authenticate_user(user_box.get(),pass_box.get()))
-    username_label=tkinter.Label(top,text="username")
-    password_label=tkinter.Label(top,text="password")
-    user_box=tkinter.Entry(top)
-    pass_box=tkinter.Entry(top)
+    top=TK.Tk()
+    top.title('GiftRegistry')
+    top.geometry('647x400') #Golden Ratio
+    username_label=TK.Label(top,text="username")
+    password_label=TK.Label(top,text="password")
+    error_label=TK.Label(top,text="")
+    user_box=TK.Entry(top)
+    pass_box=TK.Entry(top)
+    button=TK.Button(top,text="login",command=lambda:attempt_login(user_box.get(),pass_box.get(),error_label))
     username_label.pack()
     user_box.pack()
     password_label.pack()
     pass_box.pack()
     button.pack()
+    error_label.pack()
     top.mainloop()
 
-def authenticate_user(username:str,password:str):
-    raise NotImplementedError
-def displayPage():
+def attempt_login(username:str,password:str,errorOut:TK.Label):
+    if(authenticate_user(username,password)):
+        display_page()
+    else:
+        errorOut.config(text = "Incorrect username and/or password.", fg='#A33')
 
-    data=[(Mike,2),(Bob,3)]
-    root=tkinter.Tk()
+def authenticate_user(username:str,password:str):
+    return False
+    #raise NotImplementedError
+
+def display_page():
+    data=[("Mike",2),('Bob',3)]
+    root=TK.Tk()
     #for each data item insert it into the grid
     for i in range(len(data)):
             for j in range(len(data[i])):                  
-                e = tkinter.Entry(root, width=20, fg='blue',
+                e = TK.Entry(root, width=20, fg='blue',
                                font=('Arial',16,'bold'))                 
                 e.grid(row=i, column=j)
-                e.insert(tkinter.END, data[i][j])
+                e.insert(TK.END, data[i][j])
     root.mainloop()          
 
 
 if __name__ == "__main__":
-   displayPage()
+   #display_page()
+   send_login_page()
