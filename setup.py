a37a0a17fa60b8d5015f642dfb4df21ba7934be8
gabe <gabevenberg@gmail.com>
2021-12-07 19:45:36 -0600

    added module to read command line args and config.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import argparse, json, logging
+from pathlib import Path
+
+#logging.basicConfig(format='%(asctime)s:%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s:%(message)s', level=logging.DEBUG)
+
+def parse_arguments():
+    UIchoices={'GUI'}
+    parser=argparse.ArgumentParser(description='launches the GiftRegistry application')
+    parser.add_argument('configFile', type=Path, help='location of the JSON configuration file.', default='~/.config/GiftRegistry/config.json')
+    parser.add_argument('UImode', help=f'Mode to run the app in. Valid choices are: {UIchoices}', default='GUI')
+    args=parser.parse_args()
+    
+    if args.UImode not in UIchoices:
+        raise ValueError(f'UImode must be in {UIchoices}')
+    else:
+
+        logging.debug(f'{args.configFile.resolve()=}')
+        logging.debug(f'{args.UImode=}')
+        return args.configFile.resolve(), args.UImode
+
+def read_config(configFile):
+    with open(configFile, 'r') as jsonFile:
+        config = json.load(jsonFile)
+        logging.debug(f'{config=}')
+
+if __name__ == '__main__':
+    import WeddingRegistryGUI
+    configFile, UImode=parse_arguments()
+    config=read_config(configFile)
+    if UImode=='GUI':
+        WeddingRegistryGUI.send_login_page()
