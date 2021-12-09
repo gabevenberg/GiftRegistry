741ad16bd249ddd79f91db9fce9a019d5343b696
gabe <gabevenberg@gmail.com>
2021-12-09 12:16:49 -0600

    put ui mode into cfg rather than as a cli option.

--- setup.py
+++ setup.py
@@ -7,15 +7,9 @@ from pathlib import Path
 logging.basicConfig(format='%(asctime)s:%(message)s', level=logging.DEBUG)
 
 def parse_arguments():
-    UIchoices={'GUI'}
     parser=argparse.ArgumentParser(description='launches the GiftRegistry application')
     parser.add_argument('configFile', type=Path, help='location of the JSON configuration file.', default='~/.config/GiftRegistry/config.json')
-    parser.add_argument('UImode', help=f'Mode to run the app in. Valid choices are: {UIchoices}', default='GUI')
     args=parser.parse_args()
-    
-    if args.UImode not in UIchoices:
-        raise ValueError(f'UImode must be in {UIchoices}')
-    else:
 
         logging.debug(f'{args.configFile.resolve()=}')
         logging.debug(f'{args.UImode=}')
@@ -29,7 +23,7 @@ def read_config(configFile):
 
 if __name__ == '__main__':
     import WeddingRegistryGUI, databaseInteraction
-    configFile, UImode=parse_arguments()
+    configFile=parse_arguments()
     #I know global variables are discoraged, but these are the only two we will be using. (besides, these were technically already global, I just made it explicit.)
     config=read_config(configFile)
     connection=databaseInteraction.connect(
@@ -41,7 +35,7 @@ if __name__ == '__main__':
     )
     try:
         #rest of the program goes here.
-        #if UImode=='GUI':
+        #if config.uiMode=='GUI':
         #    WeddingRegistryGUI.send_login_page()
         with connection.cursor() as cur:
             cur.execute('select * from users;')
